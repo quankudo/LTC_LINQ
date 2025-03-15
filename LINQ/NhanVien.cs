91e9c128ac08a2176b33b5c8518edbfac702066f
quankudo <quannguyen2004aa@gmail.com>
2025-03-15 22:26:37 +0700

    Create Class NhanVien and Class PhongBan

---
+++ LINQ/NhanVien.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class NhanVien
+    {
+        public int id_332 { get; set; }
+        public string name_332 { get; set; }
+        public int age_332 { get; set; }
+        public int salary_332 { get; set; }
+        public int idDepartment_332 { get; set; }
+
+        public NhanVien()
+        {
+            
+        }
+
+        public NhanVien(int id_332, string name_332, int age_332, int salary_33, int idDepartment_332)
+        {
+            this.id_332 = id_332;
+            this.name_332 = name_332;
+            this.age_332 = age_332;
+            this.salary_332 = salary_332;
+            this.idDepartment_332 = idDepartment_332;
+        }
+    }
+}
---
+++ LINQ/PhongBan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class PhongBan
+    {
+        public int id_332 { get; set; }
+        public string name_332 { get; set; }
+
+        public PhongBan()
+        {
+            
+        }
+
+        public PhongBan(int id_332, string name_332)
+        {
+            this.id_332 = id_332;
+            this.name_332 = name_332;
+        }
+    }
+}
